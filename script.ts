d5e27365e36b8b69dbc14be596041d9287cef7e4
NoorFatima1818 <159902924+NoorFatima1818@users.noreply.github.com>
2024-09-08 12:33:12 -0700

    Initial commit

--- script.ts
+++ script.ts
@@ -1,9 +1,3 @@
-// function toggleAboutMe(): void {
-//     const aboutMeSection = document.getElementById("about-me");
-//     if (aboutMeSection) {
-//         aboutMeSection.style.display = aboutMeSection.style.display === "none" ? "block" : "none";
-//     }
-// }
 
 function togglemySkills(): void {
     const mySkillsSection = document.getElementById("mySkills");
